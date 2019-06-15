14ed81a4ddb6abdad41bbd484ef7227cbfae6488
roas15 <sevilla_raul@hotmail.com>
2019-06-15 09:56:43 -0500

    Hello WorlD

---
+++ files_open.py
@@ -0,0 +1,8 @@
+file = open("/Users/roas/Desktop/BootCamp Data Analytics/Module 2/3.2 Introduction to Python 2/Files/test2.txt","w") 
+ 
+file.write("Hello World") 
+file.write("This is our new text file") 
+file.write("and this is another line.") 
+file.write("Why? Because we can.") 
+ 
+file.close() 
